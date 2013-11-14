e1b8933db0dc1b52e26b9e47ea535a234c1caa2c
Thomas <Thomas@10.18.132.38>
2013-11-14 13:50:40 +0100

    Ændring

--- CandyCrusherContract/src/dk/candycrushers/control/BankManager.java
+++ CandyCrusherContract/src/dk/candycrushers/control/BankManager.java
@@ -24,7 +24,7 @@ public interface BankManager {
     CustomerDetail updateCustomer(long customerID, String firstName, String lastName, String email, String password);
 
     AccountDetail addAccount(String accountType, double balance, long customerID);
-    AccountDetail getAccount();
+    AccountDetail getAccount(long id);
     String transactionToAnOtherAccount(int fromAccountId, int toAccountId, double amount);
     AccountDetail getAccountTransactionToEachOther(long accountId);
     
