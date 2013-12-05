ad6e775387e781e0c32bac4f592f072b12a11832
Thomas <Thomas@10.18.132.38>
2013-12-05 18:22:26 +0100

    Add Bankteller virker i Client

--- Halloooo/src/halloooo/Halloooo.java
+++ Halloooo/src/halloooo/Halloooo.java
@@ -6,6 +6,7 @@ package halloooo;
 
 import com.google.gson.Gson;
 import entity.Bankteller;
+import entity.Person;
 
 /**
  *
@@ -21,24 +22,40 @@ public class Halloooo {
         CandyCustomerClient cli = new CandyCustomerClient();
         String res = cli.find_JSON(String.class, "1");
         System.out.println("Customer number 1: " + res);
+
+//        NewJerseyClient bantellerclient = new NewJerseyClient();
+//        String response = bantellerclient.find_JSON(String.class, "1");
+//        System.out.println("Bankteller number 1: " + response);
+//        
+//        
+//        Bankteller teller = new Gson().fromJson(response, Bankteller.class);
+//        
+//        
+//        teller.setBanktellerId(25);
+//        String tellerJson = new Gson().toJson(teller);
+//        System.out.println(tellerJson);
+//        
+//        //String newOne = "{\"banktellerId\":800,\"firstName\":\"Hansine\",\"lastName\":\"Hansen\",\"email\":{\"email\":\"xxx@cph.dk\",\"password\":\"q\"}}";
+//        
+//        bantellerclient.create_JSON(tellerJson);
+
+        BankTellerClient client = new BankTellerClient();
+        String find_JSON = client.find_JSON(String.class, "1");
+        System.out.println(find_JSON);
+        Bankteller fromJson = new Gson().fromJson(find_JSON, Bankteller.class);
+        System.out.println(fromJson.getFirstName());
+        fromJson.setBanktellerId(12);
         
-        NewJerseyClient bantellerclient = new NewJerseyClient();
-        String response = bantellerclient.find_JSON(String.class, "1");
-        System.out.println("Bankteller number 1: " + response);
+        //Person skal tilføjes først i databasen før bankteller kan tilføjes
+        Person person = new Person("test@test.dk", "hemlig");
+        PersonClient pclient = new PersonClient();
+        pclient.create_JSON(new Gson().toJson(person));
+        fromJson.setEmail(person);
         
-        
-        Bankteller teller = new Gson().fromJson(response, Bankteller.class);
-        
-        
-        teller.setBanktellerId(25);
-        String tellerJson = new Gson().toJson(teller);
-        System.out.println(tellerJson);
-        
-        //String newOne = "{\"banktellerId\":800,\"firstName\":\"Hansine\",\"lastName\":\"Hansen\",\"email\":{\"email\":\"xxx@cph.dk\",\"password\":\"q\"}}";
-        
-        bantellerclient.create_JSON(tellerJson);
-        
-        
-        
+        //Bankteller tilføjes
+        client.create_JSON(new Gson().toJson(fromJson));
+
+
+
     }
 }
--- SemesterProjectPart-3_Version2_(Candycrushers)/src/java/servlets/PersonServlet.java
+++ SemesterProjectPart-3_Version2_(Candycrushers)/src/java/servlets/PersonServlet.java
@@ -24,7 +24,7 @@ import model.Person;
 @WebServlet(urlPatterns = {"/PersonServlet"})
 public class PersonServlet extends HttpServlet {
 
-  Map<String,Person> persons ;
+//  Map<String,Person> persons;
   
   /**
    * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -39,31 +39,31 @@ public class PersonServlet extends HttpServlet {
           throws ServletException, IOException {
     response.setContentType("application/json;charset=UTF-8");
     String phone = request.getParameter("phone");
-    if(persons == null){
-      init();
-    }
+//    if(persons == null){
+//      init();
+//    }
     
       KrakClient krak = new KrakClient();
       String find_JSON = krak.find_JSON(String.class, phone);
     
-    Person p = persons.get(phone);
-    String json =new Gson().toJson(p);
+//    Person p = persons.get(phone);
+//    String json =new Gson().toJson(p);
     try (PrintWriter out = response.getWriter()) {
       out.println(find_JSON);
     }
   }
   
-  public void init(){
-    persons  = new HashMap();
-    
-    Address a = new Address("Nørrebrogade","Købehavn","2200");
-    Person p = new Person("Thomas","Rasmussen",25000.25d,"peter@hotmail.com",a);
-    persons.put("12345678", p);
-    
-    a = new Address("Lyngbyvej 22","Lyngby","2800");
-    p = new Person("Peter","Hansen",25000.25d,"ph@gamil.com",a);
-    persons.put("22222222", p);
-  }
+//  public void init(){
+//    persons  = new HashMap();
+//    
+//    Address a = new Address("Nørrebrogade","Købehavn","2200");
+//    Person p = new Person("Thomas","Rasmussen",25000.25d,"peter@hotmail.com",a);
+//    persons.put("12345678", p);
+//    
+//    a = new Address("Lyngbyvej 22","Lyngby","2800");
+//    p = new Person("Peter","Hansen",25000.25d,"ph@gamil.com",a);
+//    persons.put("22222222", p);
+//  }
   
 
   // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
