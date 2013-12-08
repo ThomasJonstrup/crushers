7f8d9a3b4cab9a6731fb581acbe27f2e6690f91a
Rasmus <Rasmus@10.18.132.80>
2013-12-08 12:40:57 +0100

    RKI med SQL filer

---
+++ RKI/src/conf/RKI_insert.sql
@@ -0,0 +1,10 @@
+delete from person;
+
+insert into person (PERSON_ID, EMAIL)
+values(1, 'mads.larsen@cph.dk');
+
+insert into person (PERSON_ID, EMAIL)
+values(2, 'inger.larsen@cph.dk');
+
+insert into person (PERSON_ID, EMAIL)
+values(3, 'birger.larsen@cph.dk');
---
+++ RKI/src/conf/RKI_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE Person;
+
+CREATE TABLE Person (
+  PERSON_ID int not null primary key,
+  EMAIL varchar(100)
+);
